ac4a5f9fed72d2f28214edbfdc060e43f81b732d
Alexey Semenov <alexsmn@gmail.com>
2017-06-03 18:31:57 -0600

    Revert "Cleanup client::Channel API."
    This reverts commit ab9e53d24681c9b7ed7c5fc927700dacabaa41de.
    
    # Conflicts:
    #	opcuapp/client/channel.h

--- opcuapp/client/channel.h
+++ opcuapp/client/channel.h
@@ -9,7 +9,17 @@
 namespace opcua {
 namespace client {
 
-using StatusCallback = std::function<void(StatusCode status_code)>;
+struct ChannelContext {
+  OpcUa_StringA             url;
+  OpcUa_ByteString*         client_certificate;
+  OpcUa_ByteString*         client_private_key;
+  OpcUa_ByteString*         server_certificate;
+  OpcUa_Void*               pki_config;
+  OpcUa_String*             requested_security_policy_uri;
+  OpcUa_Int32               requested_lifetime;
+  OpcUa_MessageSecurityMode message_security_mode;
+  OpcUa_UInt32              network_timeout_ms;
+};
 
 class Channel {
  public:
@@ -17,28 +27,19 @@ class Channel {
   Channel(OpcUa_Channel_SerializerType serializer_type, StatusCode& status_code);
   ~Channel();
 
-  void set_url(std::string url) { url_ = std::move(url); }
-
-  using ConnectionStateHandler = std::function<void(OpcUa_Channel_Event event)>;
-  void set_connection_state_handler(ConnectionStateHandler handler) { connection_state_handler_ = std::move(handler); }
-
-  void Connect(StatusCallback callback);
-  void Disconnect(StatusCallback callback);
-
   OpcUa_Channel handle() const { return handle_; }
 
- private:
+  using ConnectionStateHandler = std::function<void(StatusCode status_code, OpcUa_Channel_Event event)>;
+  void Connect(const ChannelContext& context, const ConnectionStateHandler& connection_state_handler);
+
   void Reset();
 
+ private:
   static OpcUa_StatusCode ConnectionStateChangedHelper(OpcUa_Channel hChannel, OpcUa_Void* pCallbackData,
       OpcUa_Channel_Event eEvent, OpcUa_StatusCode uStatus);
 
-  std::string url_;
-  ConnectionStateHandler connection_state_handler_;
-
   OpcUa_Channel handle_ = OpcUa_Null;
-  StatusCallback connect_callback_;
-  StatusCallback disconnect_callback_;
+  ConnectionStateHandler connection_state_handler_;
 };
 
 inline Channel::Channel(OpcUa_Channel_SerializerType serializer_type) {
@@ -50,65 +51,44 @@ inline Channel::Channel(OpcUa_Channel_SerializerType serializer_type, StatusCode
 }
 
 inline Channel::~Channel() {
-  OpcUa_Channel_Delete(&handle_);
+  if (handle_)
+    OpcUa_Channel_Delete(&handle_);
 }
 
-inline void Channel::Connect(StatusCallback callback) {
-  assert(!connect_callback_);
-  connect_callback_ = std::move(callback);
+inline void Channel::Connect(const ChannelContext& context, const ConnectionStateHandler& connection_state_handler) {
+  connection_state_handler_ = connection_state_handler;
 
-  ByteString client_private_key;
-  OpcUa_P_OpenSSL_CertificateStore_Config pki_config{OpcUa_NO_PKI};
-  String requested_security_policy_uri{OpcUa_SecurityPolicy_None};
-
-  StatusCode status_code = ::OpcUa_Channel_BeginConnect(handle_,
-      const_cast<OpcUa_StringA>(url_.c_str()),
-      nullptr,
-      client_private_key.pass(),
-      nullptr,
-      &pki_config,
-      requested_security_policy_uri.pass(),
-      0,
-      OpcUa_MessageSecurityMode_None,
-      10000,
+  StatusCode status_code = OpcUa_Channel_BeginConnect(handle_,
+      context.url,
+      context.client_certificate,
+      context.client_private_key,
+      context.server_certificate,
+      context.pki_config,
+      context.requested_security_policy_uri,
+      context.requested_lifetime,
+      context.message_security_mode,
+      context.network_timeout_ms,
       &Channel::ConnectionStateChangedHelper,
       this);
 
-  if (!status_code && connect_callback_)
-    connect_callback_(status_code);
-}
-
-inline void Channel::Disconnect(StatusCallback callback) {
-  assert(!disconnect_callback_);
-  disconnect_callback_ = std::move(callback);
-
-  StatusCode status_code = ::OpcUa_Channel_BeginDisconnect(handle_, &Channel::ConnectionStateChangedHelper, this);
-  if (!status_code && disconnect_callback_)
-    disconnect_callback_(status_code);
+  if (!status_code) {
+    connection_state_handler_ = nullptr;
+    connection_state_handler(status_code, eOpcUa_Channel_Event_Disconnected);
+  }
 }
 
 inline void Channel::Reset() {
   // TODO:
-  connect_callback_ = nullptr;
-  disconnect_callback_ = nullptr;
 }
 
 // static
 inline OpcUa_StatusCode Channel::ConnectionStateChangedHelper(OpcUa_Channel hChannel, OpcUa_Void* pCallbackData,
     OpcUa_Channel_Event eEvent, OpcUa_StatusCode uStatus) {
   auto& channel = *static_cast<Channel*>(pCallbackData);
-  if (channel.connection_state_handler_)
-    channel.connection_state_handler_(eEvent);
-  if (channel.connect_callback_) {
-    auto connect_callback = std::move(channel.connect_callback_);
-    channel.connect_callback_ = nullptr;
-    connect_callback(uStatus);
-  }
-  if (channel.disconnect_callback_) {
-    auto disconnect_callback = std::move(channel.disconnect_callback_);
-    channel.disconnect_callback_ = nullptr;
-    disconnect_callback(uStatus);
-  }
+  auto handler = channel.connection_state_handler_;
+  if (eEvent == eOpcUa_Channel_Event_Disconnected)
+    channel.connection_state_handler_ = nullptr;
+  handler(uStatus, eEvent);
   return OpcUa_Good;
 }
 
