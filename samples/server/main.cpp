420055dec383768926651ea3c68e9ce04263595c
alexsmn <alexsmn@gmail.com>
2018-09-24 20:14:42 -0600

    Format sample server source code.

--- samples/server/main.cpp
+++ samples/server/main.cpp
@@ -1,13 +1,13 @@
-#include <fstream>
-#include <iostream>
 #include <opcuapp/extension_object.h>
 #include <opcuapp/platform.h>
 #include <opcuapp/proxy_stub.h>
 #include <opcuapp/requests.h>
 #include <opcuapp/server/endpoint.h>
 #include <opcuapp/server/node_loader.h>
-#include <opcuapp/vector.h>
 #include <opcuapp/timer.h>
+#include <opcuapp/vector.h>
+#include <fstream>
+#include <iostream>
 #include <thread>
 
 const char kPredefinedNodesPath[] = "Opc.Ua.PredefinedNodes.uanodes";
@@ -16,15 +16,18 @@ namespace {
 
 class Variable : public opcua::server::MonitoredItem {
  public:
-  using ReadHandler = std::function<opcua::DataValue(opcua::AttributeId attribute_id)>;
+  using ReadHandler =
+      std::function<opcua::DataValue(opcua::AttributeId attribute_id)>;
 
   explicit Variable(ReadHandler handler) : read_handler_{std::move(handler)} {}
 
   opcua::DataValue Read(opcua::AttributeId attribute_id) const;
 
   // opcua::server::MonitoredItem
-  virtual void SubscribeDataChange(const opcua::server::DataChangeHandler& data_change_handler) override;
-  virtual void SubscribeEvents(const opcua::server::EventHandler& event_handler) override;
+  virtual void SubscribeDataChange(
+      const opcua::server::DataChangeHandler& data_change_handler) override;
+  virtual void SubscribeEvents(
+      const opcua::server::EventHandler& event_handler) override;
 
  private:
   const ReadHandler read_handler_;
@@ -36,7 +39,8 @@ opcua::DataValue Variable::Read(opcua::AttributeId attribute_id) const {
   return read_handler_(attribute_id);
 }
 
-void Variable::SubscribeDataChange(const opcua::server::DataChangeHandler& data_change_handler) {
+void Variable::SubscribeDataChange(
+    const opcua::server::DataChangeHandler& data_change_handler) {
   {
     auto data_value = Read(OpcUa_Attributes_Value);
     if (data_value.status_code())
@@ -53,11 +57,12 @@ void Variable::SubscribeDataChange(const opcua::server::DataChangeHandler& data_
   });
 }
 
-void Variable::SubscribeEvents(const opcua::server::EventHandler& event_handler) {
+void Variable::SubscribeEvents(
+    const opcua::server::EventHandler& event_handler) {
   assert(false);
 }
 
-} // namespace
+}  // namespace
 
 class Server {
  public:
@@ -65,7 +70,8 @@ class Server {
 
  private:
   std::shared_ptr<Variable> GetVariable(const OpcUa_NodeId& node_id) const;
-  const opcua::server::NodeState* GetStaticNode(const OpcUa_NodeId& node_id) const;
+  const opcua::server::NodeState* GetStaticNode(
+      const OpcUa_NodeId& node_id) const;
 
   opcua::DataValue Read(const OpcUa_ReadValueId& read_value_id) const;
 
@@ -76,8 +82,10 @@ class Server {
 
   const opcua::ByteString server_certificate_;
   const opcua::server::Endpoint::SecurityPolicyConfiguration security_policy_;
-  const OpcUa_Key server_private_key{OpcUa_Crypto_KeyType_Invalid, {0, (OpcUa_Byte*)""}};
-  const OpcUa_P_OpenSSL_CertificateStore_Config pki_config_{OpcUa_NO_PKI, OpcUa_Null, OpcUa_Null,OpcUa_Null, 0, OpcUa_Null};
+  const OpcUa_Key server_private_key{OpcUa_Crypto_KeyType_Invalid,
+                                     {0, (OpcUa_Byte*)""}};
+  const OpcUa_P_OpenSSL_CertificateStore_Config pki_config_{
+      OpcUa_NO_PKI, OpcUa_Null, OpcUa_Null, OpcUa_Null, 0, OpcUa_Null};
   opcua::server::Endpoint endpoint_{OpcUa_Endpoint_SerializerType_Binary};
 
   const opcua::DateTime start_time_ = opcua::DateTime::UtcNow();
@@ -89,34 +97,42 @@ class Server {
 Server::Server() {
   std::ifstream stream{kPredefinedNodesPath, std::ios::in | std::ios::binary};
   if (!stream)
-    throw std::runtime_error{std::string{"Can't open file "} + kPredefinedNodesPath};
+    throw std::runtime_error{std::string{"Can't open file "} +
+                             kPredefinedNodesPath};
 
-  for (auto& static_node : opcua::server::LoadPredefinedNodes(namespace_uris_, stream)) {
+  for (auto& static_node :
+       opcua::server::LoadPredefinedNodes(namespace_uris_, stream)) {
     opcua::NodeId node_id = static_node.node_id;
     static_nodes_.emplace(std::move(node_id), std::move(static_node));
   }
 
-  variables_.emplace(OpcUaId_Server_ServerStatus,
-      std::make_shared<Variable>([this](opcua::AttributeId attribute_id) -> opcua::DataValue {
-        if (attribute_id != OpcUa_Attributes_Value)
-          return OpcUa_Bad;
-        std::cout << "Read server status" << std::endl;
-        const auto time = opcua::DateTime::UtcNow();
-        opcua::ServerStatusDataType server_status;
-        server_status.CurrentTime = time.get();
-        server_status.State = OpcUa_ServerState_Running;
-        server_status.StartTime = start_time_.get();
-        return {OpcUa_Good, opcua::ExtensionObject::Encode(std::move(server_status)), time, time};
-      }));
+  variables_.emplace(
+      OpcUaId_Server_ServerStatus,
+      std::make_shared<Variable>(
+          [this](opcua::AttributeId attribute_id) -> opcua::DataValue {
+            if (attribute_id != OpcUa_Attributes_Value)
+              return OpcUa_Bad;
+            std::cout << "Read server status" << std::endl;
+            const auto time = opcua::DateTime::UtcNow();
+            opcua::ServerStatusDataType server_status;
+            server_status.CurrentTime = time.get();
+            server_status.State = OpcUa_ServerState_Running;
+            server_status.StartTime = start_time_.get();
+            return {OpcUa_Good,
+                    opcua::ExtensionObject::Encode(std::move(server_status)),
+                    time, time};
+          }));
 
-  variables_.emplace(OpcUaId_Server_ServerStatus_CurrentTime,
-      std::make_shared<Variable>([](opcua::AttributeId attribute_id) -> opcua::DataValue {
-        if (attribute_id != OpcUa_Attributes_Value)
-          return OpcUa_Bad;
-        std::cout << "Read current time" << std::endl;
-        const auto time = opcua::DateTime::UtcNow();
-        return {OpcUa_Good, time, time, time};
-      }));
+  variables_.emplace(
+      OpcUaId_Server_ServerStatus_CurrentTime,
+      std::make_shared<Variable>(
+          [](opcua::AttributeId attribute_id) -> opcua::DataValue {
+            if (attribute_id != OpcUa_Attributes_Value)
+              return OpcUa_Bad;
+            std::cout << "Read current time" << std::endl;
+            const auto time = opcua::DateTime::UtcNow();
+            return {OpcUa_Good, time, time, time};
+          }));
 
   endpoint_.set_status_handler([](opcua::server::Endpoint::Event event) {
     switch (event) {
@@ -141,30 +157,36 @@ Server::Server() {
     }
   });
 
-  endpoint_.set_read_handler([this](OpcUa_ReadRequest& request, const opcua::server::ReadCallback& callback) {
-    opcua::Span<OpcUa_ReadValueId> read_value_ids{request.NodesToRead, static_cast<size_t>(request.NoOfNodesToRead)};
-    std::cout << "Read " << read_value_ids.size() << " values" << std::endl;
+  endpoint_.set_read_handler(
+      [this](OpcUa_ReadRequest& request,
+             const opcua::server::ReadCallback& callback) {
+        opcua::Span<OpcUa_ReadValueId> read_value_ids{
+            request.NodesToRead, static_cast<size_t>(request.NoOfNodesToRead)};
+        std::cout << "Read " << read_value_ids.size() << " values" << std::endl;
 
-    opcua::Vector<OpcUa_DataValue> results(read_value_ids.size());
-    for (size_t i = 0; i < read_value_ids.size(); ++i)
-      Read(read_value_ids[i]).release(results[i]);
+        opcua::Vector<OpcUa_DataValue> results(read_value_ids.size());
+        for (size_t i = 0; i < read_value_ids.size(); ++i)
+          Read(read_value_ids[i]).release(results[i]);
 
-    opcua::ReadResponse response;
-    response.ResponseHeader.ServiceResult = OpcUa_Good;
-    response.NoOfResults = results.size();
-    response.Results = results.release();
-    callback(response);
-  });
+        opcua::ReadResponse response;
+        response.ResponseHeader.ServiceResult = OpcUa_Good;
+        response.NoOfResults = results.size();
+        response.Results = results.release();
+        callback(response);
+      });
 
-  endpoint_.set_browse_handler([](OpcUa_BrowseRequest& request, const opcua::server::BrowseCallback& callback) {
-    std::cout << "Browse" << std::endl;
-    opcua::BrowseResponse response;
-    response.ResponseHeader.ServiceResult = OpcUa_Bad;
-    callback(response);
-  });
+  endpoint_.set_browse_handler(
+      [](OpcUa_BrowseRequest& request,
+         const opcua::server::BrowseCallback& callback) {
+        std::cout << "Browse" << std::endl;
+        opcua::BrowseResponse response;
+        response.ResponseHeader.ServiceResult = OpcUa_Bad;
+        callback(response);
+      });
 
   endpoint_.set_create_monitored_item_handler(
-      [this](opcua::ReadValueId& read_value_id) -> opcua::server::CreateMonitoredItemResult {
+      [this](opcua::ReadValueId& read_value_id)
+          -> opcua::server::CreateMonitoredItemResult {
         std::cout << "CreateMonitoredItem" << std::endl;
         auto variable = GetVariable(read_value_id.NodeId);
         if (!variable)
@@ -173,16 +195,18 @@ Server::Server() {
       });
 
   opcua::String url = "opc.tcp://localhost:4840";
-  endpoint_.Open(std::move(url), true, server_certificate_.get(), server_private_key, &pki_config_,
-      {&security_policy_, 1});
+  endpoint_.Open(std::move(url), true, server_certificate_.get(),
+                 server_private_key, &pki_config_, {&security_policy_, 1});
 }
 
-std::shared_ptr<Variable> Server::GetVariable(const OpcUa_NodeId& node_id) const {
+std::shared_ptr<Variable> Server::GetVariable(
+    const OpcUa_NodeId& node_id) const {
   auto i = variables_.find(node_id);
   return i != variables_.end() ? i->second : nullptr;
 }
 
-const opcua::server::NodeState* Server::GetStaticNode(const OpcUa_NodeId& node_id) const {
+const opcua::server::NodeState* Server::GetStaticNode(
+    const OpcUa_NodeId& node_id) const {
   auto i = static_nodes_.find(node_id);
   return i != static_nodes_.end() ? &i->second : nullptr;
 }
